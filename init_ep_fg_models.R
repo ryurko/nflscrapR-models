2876deaaafc64c21705bbef041dc4f0d5650bbd2
ryurko <ryurko21@gmail.com>
2018-06-02 16:25:16 -0400

    Edit file for generating the expected points and field goal models used in nflscrapR by cleaning code and providing code for where data was saved in repository

--- init_ep_fg_models.R
+++ init_ep_fg_models.R
@@ -208,7 +208,8 @@ pbp_ep_model_data <- pbp_ep_model_data %>%
          # Create a variable that is time remaining until end of half:
          # (only working with up to 2016 data so can ignore 2017 time change)
          TimeSecs_Remaining = ifelse(qtr %in% c(1,2), TimeSecs - 1800,
-                                      ifelse(qtr == 5, TimeSecs + 900, TimeSecs)),
+                                      ifelse(qtr == 5, TimeSecs + 900, 
+                                             TimeSecs)),
          
          # log transform of yards to go and indicator for two minute warning:
          log_ydstogo = log(ydstogo),
@@ -238,122 +239,39 @@ pbp_ep_model_data <- pbp_ep_model_data %>%
 # write_csv(pbp_ep_model_data, "data/pbp_ep_model_data.csv")
 
 # Fit the expected points model:
-ep_model <- nnet::multinom(Next_Score_Half ~ TimeSecs_Remaining + yrdline100 + down + log_ydstogo + 
-                           GoalToGo + log_ydstogo*down + yrdline100*down + GoalToGo*log_ydstogo + Under_TwoMinute_Warning, 
-                           data = pbp_ep_model_data, weights = Total_W_Scaled, maxit = 300)
+# install.packages("nnet")
+ep_model <- nnet::multinom(Next_Score_Half ~ TimeSecs_Remaining + yrdline100 + 
+                           down + log_ydstogo + GoalToGo + log_ydstogo*down + 
+                           yrdline100*down + GoalToGo*log_ydstogo + 
+                           Under_TwoMinute_Warning, data = pbp_ep_model_data, 
+                           weights = Total_W_Scaled, maxit = 300)
 
 # Save the model (commented out due to file size limit)
 # save(ep_model, file="ep_model.RData")
 
-# Following code is used to build the models that hold
-# the season out for predictions, and weights the observations
-# by the difference in Season as well
-
-# Define the build_LOSO_EP_Model() function that takes in
-# season to holdout and the data:
-
-build_LOSO_EP_Model <- function(holdout_season, data = pbp_ep_model_data){
-  # Filter out the holdout season:
-  train <- data %>%
-    filter(Season != holdout_season)
-  
-  # Generate the weights
-  
-  # Create a weight column based on difference in drives between play and next score:
-  train <- train %>% 
-    mutate(Drive_Score_Dist_W = (max(Drive_Score_Dist) - Drive_Score_Dist) / (max(Drive_Score_Dist) - min(Drive_Score_Dist)),
-           # Create a weight column based on score differential:
-           ScoreDiff_W = (max(abs(ScoreDiff)) - abs(ScoreDiff)) / (max(abs(ScoreDiff)) - min(abs(ScoreDiff))),
-           # Create a weight column based on season difference from holdout season:
-           Season_Diff = abs(Season - holdout_season),
-           Season_Diff_W = (max(Season_Diff) - Season_Diff) / (max(Season_Diff) - min(Season_Diff)),
-           # Add the three together and scale again:
-           Total_Season_W = Drive_Score_Dist_W + ScoreDiff_W + Season_Diff_W,
-           Total_Season_W_Scaled = (Total_Season_W - min(Total_Season_W)) / (max(Total_Season_W) - min(Total_Season_W)))
-  
-  # Build model:
-  ep_model <- nnet::multinom(Next_Score_Half ~ TimeSecs_Remaining + yrdline100 + down + log_ydstogo + 
-                             GoalToGo + log_ydstogo*down + yrdline100*down + GoalToGo*log_ydstogo + Under_TwoMinute_Warning, 
-                             data = train, weights = Total_Season_W_Scaled, maxit = 300)
-  
-  # Return
-  return(ep_model)
-}
-
-# Fit each season's model by holding out that season:
-
-ep_model_09 <- build_LOSO_EP_Model(2009)
-ep_model_10 <- build_LOSO_EP_Model(2010)
-ep_model_11 <- build_LOSO_EP_Model(2011)
-ep_model_12 <- build_LOSO_EP_Model(2012)
-ep_model_13 <- build_LOSO_EP_Model(2013)
-ep_model_14 <- build_LOSO_EP_Model(2014)
-ep_model_15 <- build_LOSO_EP_Model(2015)
-ep_model_16 <- build_LOSO_EP_Model(2016)
-
-# Code to save the models is commented out because of the push limit:
-# save(ep_model_09, file="ep_model_09.RData")
-# save(ep_model_10, file="ep_model_10.RData")
-# save(ep_model_11, file="ep_model_11.RData")
-# save(ep_model_12, file="ep_model_12.RData")
-# save(ep_model_13, file="ep_model_13.RData")
-# save(ep_model_14, file="ep_model_14.RData")
-# save(ep_model_15, file="ep_model_15.RData")
-# save(ep_model_16, file="ep_model_16.RData")
-
 # ----------------------------------------------------------------------------
 
-# Filter the pbp_data_next_score dataset to the rows needed for the field goal model:
+# Filter the pbp_data_next_score dataset for the field goal model:
+fg_model_data <-  pbp_data_next_score %>% 
+  filter(PlayType %in% c("Field Goal","Extra Point","Run") & 
+           (!is.na(ExPointResult) | !is.na(FieldGoalResult)))
 
-fg_data <-  pbp_data_next_score %>% 
-  filter(PlayType %in% c("Field Goal","Extra Point","Run") & (!is.na(ExPointResult) | !is.na(FieldGoalResult)))
+nrow(fg_model_data)
 # 16906
 
+# Save dataset in data folder as fg_model_data.csv
+# (NOTE: this dataset is not pushed due to its size exceeding
+# the github limit but will be referenced in other files)
+# write_csv(fg_model_data, "data/fg_model_data.csv")
+
 # Fit the field goal model:
-fg_model <- mgcv::bam(sp ~ s(yrdline100), data = fg_data, family = "binomial")
+# install.packages("mgcv")
+fg_model <- mgcv::bam(sp ~ s(yrdline100), 
+                      data = fg_model_data, family = "binomial")
 
 # Save the model (commented out due to file size limit)
 # save(fg_model, file="fg_model.RData")
 
-# Following code is used to build the models that hold
-# the season out for predictions, and weights the observations
-# by the difference in Season as well
-
-# Define the build_LOSO_FG_Model() function that takes in
-# season to holdout and the data:
-
-build_LOSO_FG_Model <- function(holdout_season, data = fg_data){
-  # Filter out the holdout season and down to the :
-  train <- data %>%
-    filter(Season != holdout_season)
-  
-  # Build model:
-  fg_model <- mgcv::bam(sp ~ s(yrdline100),data=train,family="binomial")
-
-  # Return
-  return(fg_model)
-}
-
-# Fit each season's model by holding out that season:
-
-fg_model_09 <- build_LOSO_FG_Model(2009)
-fg_model_10 <- build_LOSO_FG_Model(2010)
-fg_model_11 <- build_LOSO_FG_Model(2011)
-fg_model_12 <- build_LOSO_FG_Model(2012)
-fg_model_13 <- build_LOSO_FG_Model(2013)
-fg_model_14 <- build_LOSO_FG_Model(2014)
-fg_model_15 <- build_LOSO_FG_Model(2015)
-fg_model_16 <- build_LOSO_FG_Model(2016)
-
-# Code to save the models is commented out because of the push limit:
-# save(fg_model_09, file="fg_model_09.RData")
-# save(fg_model_10, file="fg_model_10.RData")
-# save(fg_model_11, file="fg_model_11.RData")
-# save(fg_model_12, file="fg_model_12.RData")
-# save(fg_model_13, file="fg_model_13.RData")
-# save(fg_model_14, file="fg_model_14.RData")
-# save(fg_model_15, file="fg_model_15.RData")
-# save(fg_model_16, file="fg_model_16.RData")
 
 
 
