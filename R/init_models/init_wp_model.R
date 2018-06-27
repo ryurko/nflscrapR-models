4a2f0146e307380bc53ef088b3b33fda63ee9eb8
ryurko <ryurko21@gmail.com>
2018-06-27 01:27:12 -0400

    Fix typo in nflWAR install line

--- R/init_models/init_wp_model.R
+++ R/init_models/init_wp_model.R
@@ -6,7 +6,7 @@ library(tidyverse)
 
 # Access nflWAR:
 # install.packages("devtools")
-# devtools::install.packages("ryurko/nflWAR")
+# devtools::install_github("ryurko/nflWAR")
 
 library(nflWAR)
 
